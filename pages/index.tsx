8d8ca530430ef1a1814a6fb8e1413220c58792be
Fabien Caylus <fabien.caylus@gmail.com>
2021-03-20 20:11:38 +0100

    Fix front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
-import { Box, IconButton, Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import Page from '../src/components/Page';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { GitHub } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     title: {
       paddingBottom: theme.spacing(4),
-    },
-    github: {
-      position: 'absolute',
-      margin: theme.spacing(2),
-      bottom: 0,
-      right: 0,
     },
   })
 );
@@ -22,21 +15,12 @@ export default function Home() {
 
   return (
     <Page homepage name="">
-      <Box m={4} my={4}>
+      <Box m={4}>
         <Typography variant="h4" component="h1" className={classes.title}>
           toolbelt.sh
         </Typography>
         <Typography variant="body1">The only web development tool belt you'll ever need.</Typography>
       </Box>
-
-      <IconButton
-        className={classes.github}
-        href="https://github.com/fcaylus/toolbelt.sh"
-        target="_blank"
-        rel="noopener"
-      >
-        <GitHub fontSize="large" />
-      </IconButton>
     </Page>
   );
 }
